e644780517cad9de3eeab64ed57030eac3d3b1f5
alef-ribeiro <alef.ribeiro@dcomp.ufs.br>
2020-05-19 22:17:43 -0300

    enviando projeto para o git

---
+++ scripts.js
@@ -0,0 +1,22 @@
+function onOff() {
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("hide")
+
+
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("addScroll")
+
+
+
+    document
+        .querySelector("body#modal")
+        .classList
+        .toggle("hideScroll")
+
+
+
+}
